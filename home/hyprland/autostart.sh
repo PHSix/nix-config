53b3753c8814f0d1b6bda3b35d2918e1cc6ab3f2
ph <chenyi.ph@outlook.com>
2023-05-12 16:03:36 +0800

    add mako on start

--- home/hyprland/autostart.sh
+++ home/hyprland/autostart.sh
@@ -1,2 +1,6 @@
 nm-applet &
 python3 ~/.config/hypr/scripts/swwl.py &
+udiskie &
+fcitx5 &
+waybar &
+mako &
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,6 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
   xdg.configFile."hypr/autostart.sh".source = ./autostart.sh;
   home.file.".config/hypr/scripts/swwl.py".source = ./swwl.py;
+
+  home.packages = [
+    pkgs.mako
+  ];
 }
 
