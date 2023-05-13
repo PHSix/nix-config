c3b6a7ad23c37971f458ca705ea9d0d15f9c049c
ph <chenyi.ph@outlook.com>
2023-05-13 14:11:55 +0800

    comment docker daemon settings

--- module/misc.nix
+++ module/misc.nix
@@ -13,9 +13,9 @@
 
   # enable docker service
   virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings = {
-    registry-mirrors = [ "https://registry.docker-cn.com" ];
-  };
+  # virtualisation.docker.daemon.settings = {
+  #   registry-mirrors = [ "https://registry.docker-cn.com" ];
+  # };
 
   environment.pathsToLink = [ "/share/zsh" ];
 }
