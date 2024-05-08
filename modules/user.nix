80694ac266c8415b4a0652ea1cf0e6c2b708d5d9
ph <chenyi.ph@outlook.com>
2024-05-08 10:09:38 +0800

    add virtualisation.nix

--- modules/user.nix
+++ modules/user.nix
@@ -3,6 +3,6 @@
   security.doas.enable = true;
   users.users."${username}" = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "dialout" "docker" "seat" ];
+    extraGroups = [ "networkmanager" "wheel" "dialout" "docker" "seat" "libvirtd" ];
   };
 }
---
+++ modules/virtualisation.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: {
+  # virtualisation.libvirtd.enable = true;
+  # programs.virt-manager.enable = true;
+
+  # enable docker service
+  # virtualisation.docker.enable = true;
+  # virtualisation.docker.daemon.settings = {
+  #   registry-mirrors = [ "https://registry.docker-cn.com" ];
+  # };
+
+  # enable podman service
+  # virtualisation.podman.enable = true;
+}
