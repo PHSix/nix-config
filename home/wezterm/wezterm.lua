7375fc0de4057968956bbf7e1ed08b45578c5607
ph <chenyi.ph@outlook.com>
2024-06-12 11:27:08 +0800

    update wez config

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -3,22 +3,24 @@ local act = wezterm.action
 
 return {
 	font = wezterm.font_with_fallback({
-		"Macon", -- "Monaco"
+		"JetBrains Mono NL", -- "Macon", -- "Monaco"
+		"Macon",
 		"Symbols Nerd Font",
 		"Noto Color Emoji",
 		"霞鹜文楷",
 		"monospace",
 	}),
 	audible_bell = "Disabled",
-	font_size = 12,
+	font_size = 13,
 	enable_tab_bar = false,
 	color_scheme = "GitHub Dark",
 	enable_wayland = true,
+	window_background_opacity = 0.9,
 	window_padding = {
 		left = 0,
 		right = 0,
-		top = 0,
-		bottom = 0,
+		top = 6,
+		bottom = 6,
 	},
 	check_for_updates = false,
 	window_close_confirmation = "NeverPrompt",
