9454ef35f31f270716e23c829e7aa54bd78c9c7f
ph <chenyi.ph@outlook.com>
2024-12-18 11:09:06 +0800

    use joshuto instead of yazi

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -40,18 +40,34 @@
         }
       ];
       initExtra = ''
-        if type "yazi" > /dev/null; then
-          alias ra="yz"
-          function yz() {
-            tmp="$(mktemp -t "yazi-cwd.XXXXX")"
-            yazi --cwd-file="$tmp"
-            if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-              cd -- "$cwd"
-            fi
-            rm -f -- "$tmp"
+        if type "joshuto" > /dev/null; then
+          alias ra="joshuto"
+          alias yz="joshuto"
+          function joshuto() {
+            ID="$$"
+            mkdir -p /tmp/$USER
+            OUTPUT_FILE="/tmp/$USER/joshuto-cwd-$ID"
+            env joshuto --output-file "$OUTPUT_FILE" $@
+            exit_code=$?
+
+            case "$exit_code" in
+              # regular exit
+              0)
+                ;;
+              # output contains current directory
+              101)
+                JOSHUTO_CWD=$(cat "$OUTPUT_FILE")
+                cd "$JOSHUTO_CWD"
+                ;;
+              # output selected files
+              102)
+                ;;
+              *)
+                echo "Exit code: $exit_code"
+                ;;
+            esac
           }
         fi
-
         function proxy(){
           export https_proxy=http://localhost:7897
           export http_proxy=http://localhost:7897
