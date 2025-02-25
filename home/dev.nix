674bc3ab23831a96fbc27c826143d8b320cdf9ad
ph <chenyi.ph@outlook.com>
2025-02-26 07:54:21 +0800

    remove default rs proxy

--- home/dev.nix
+++ home/dev.nix
@@ -27,20 +27,6 @@ in
     ++ f useGo (with pkgs; [ go ])
   ;
 
-  home.file.".cargo/config.toml" = {
-    text = ''
-      [source.crates-io]
-      replace-with = 'rsproxy-sparse'
-      [source.rsproxy]
-      registry = "https://rsproxy.cn/crates.io-index"
-      [source.rsproxy-sparse]
-      registry = "sparse+https://rsproxy.cn/index/"
-      [registries.rsproxy]
-      index = "https://rsproxy.cn/crates.io-index"
-      [net]
-      git-fetch-with-cli = true
-    '';
-  };
   home.file.".npmrc" =
     {
       text = ''
