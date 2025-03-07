44fa860c464081e0cbcb5aa1b0ce7e7040f5c0fe
ph <chenyi.ph@outlook.com>
2025-03-07 19:50:34 +0800

    use tuna mirror

--- deps/nixos.nix
+++ deps/nixos.nix
@@ -9,7 +9,7 @@
 
     settings = {
       substituters = [
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       ];
 
