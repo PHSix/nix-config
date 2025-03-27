62ddd81fa5e363022d275437fb3718ea2d4abd0c
ph <chenyi.ph@outlook.com>
2025-03-27 21:57:49 +0800

    remove maple font

--- deps/locale.nix
+++ deps/locale.nix
@@ -1,11 +1,19 @@
 # for language
 #     font config
 #     input method
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
-    supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+    supportedLocales = [
+      "zh_CN.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
     inputMethod = {
       enable = true;
       type = "fcitx5";
@@ -31,8 +39,7 @@
     sessionVariables = {
       # to fix chrome or edge which core is chormium v8 case input trrigger unstable on wayland
       # this solution is copy from: https://github.com/NixOS/nixpkgs/issues/129442#issuecomment-875972207
-      NIX_PROFILES = "${builtins.concatStringsSep " "
-        (lib.reverseList config.environment.profiles)}";
+      NIX_PROFILES = "${builtins.concatStringsSep " " (lib.reverseList config.environment.profiles)}";
       # to fix gtk application start slow problem, like waybar and clash-verge
       GTK_USE_PORTAL = "0";
     };
@@ -47,7 +54,10 @@
         autohint = false;
         # hintstyle = "hintslight(10px,12px)";
       };
-      defaultFonts.emoji = [ "Twitter Color Emoji" "Noto Color Emoji" ];
+      defaultFonts.emoji = [
+        "Twitter Color Emoji"
+        "Noto Color Emoji"
+      ];
       defaultFonts.monospace = [
         "LXGW WenKai Mono"
         "UbuntuMono Nerd Font Mono"
@@ -55,17 +65,24 @@
         "Noto Sans CJK SC"
         "Sarasa Mono SC"
       ];
-      defaultFonts.sansSerif =
-        [ "LXGW WenKai Mono" "Noto Sans CJK SC" "DejaVu Sans" ];
-      defaultFonts.serif = [ "LXGW WenKai Mono" "Noto Sans CJK SC" ];
+      defaultFonts.sansSerif = [
+        "LXGW WenKai Mono"
+        "Noto Sans CJK SC"
+        "DejaVu Sans"
+      ];
+      defaultFonts.serif = [
+        "LXGW WenKai Mono"
+        "Noto Sans CJK SC"
+      ];
 
       localConf = ''
         <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
         <fontconfig>
           <!-- Use language-specific font variants. -->
-          ${
-            lib.concatMapStringsSep "\n" ({ lang, variant }:
+          ${lib.concatMapStringsSep "\n"
+            (
+              { lang, variant }:
               let
                 replace = from: to: ''
                   <match target="pattern">
@@ -80,25 +97,28 @@
                     </edit>
                   </match>
                 '';
-              in replace "Noto Sans CJK SC" "Noto Sans CJK ${variant}"
-              + replace "Noto Serif CJK SC" "Noto Serif CJK ${variant}") [
-                {
-                  lang = "zh-TW";
-                  variant = "TC";
-                }
-                {
-                  lang = "zh-HK";
-                  variant = "HK";
-                }
-                {
-                  lang = "ja";
-                  variant = "JP";
-                }
-                {
-                  lang = "ko";
-                  variant = "KR";
-                }
-              ]
+              in
+              replace "Noto Sans CJK SC" "Noto Sans CJK ${variant}"
+              + replace "Noto Serif CJK SC" "Noto Serif CJK ${variant}"
+            )
+            [
+              {
+                lang = "zh-TW";
+                variant = "TC";
+              }
+              {
+                lang = "zh-HK";
+                variant = "HK";
+              }
+              {
+                lang = "ja";
+                variant = "JP";
+              }
+              {
+                lang = "ko";
+                variant = "KR";
+              }
+            ]
           }
         </fontconfig>
       '';
@@ -106,37 +126,39 @@
 
     fontDir.enable = true;
     enableGhostscriptFonts = true;
-    packages = (with pkgs; [
-      fira
-      fira-code
-      # symbola # 特殊字符
-      jetbrains-mono
-      font-awesome
+    packages =
+      (with pkgs; [
+        fira
+        fira-code
+        # symbola # 特殊字符
+        jetbrains-mono
+        font-awesome
 
-      noto-fonts-color-emoji
-      rofi-emoji
-      twitter-color-emoji
-      unicode-emoji
-      vista-fonts # many windows font(include consolas)
-      maple-mono-NF
-      monego
+        noto-fonts-color-emoji
+        rofi-emoji
+        twitter-color-emoji
+        unicode-emoji
+        vista-fonts # many windows font(include consolas)
+        # maple-mono-NF
+        monego
 
-      cascadia-code
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      # sarasa-gothic
-      noto-fonts
-      wqy_microhei
-      wqy_zenhei
-      lxgw-wenkai
-      intel-one-mono
-      icursive-nerd-font
-    ]) ++ [
-      pkgs.nerd-fonts.fira-code
-      pkgs.nerd-fonts.fira-mono
-      pkgs.nerd-fonts.ubuntu-mono
-      pkgs.nerd-fonts.symbols-only
-      pkgs.nerd-fonts.hack
-    ];
+        cascadia-code
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        # sarasa-gothic
+        noto-fonts
+        wqy_microhei
+        wqy_zenhei
+        lxgw-wenkai
+        intel-one-mono
+        icursive-nerd-font
+      ])
+      ++ [
+        pkgs.nerd-fonts.fira-code
+        pkgs.nerd-fonts.fira-mono
+        pkgs.nerd-fonts.ubuntu-mono
+        pkgs.nerd-fonts.symbols-only
+        pkgs.nerd-fonts.hack
+      ];
   };
 }
