de3a2395b7d275cda1097c70e44300450c979f47
ph <chenyi.ph@outlook.com>
2025-04-06 21:46:58 +0800

    format gnome

--- deps/gnome.nix
+++ deps/gnome.nix
@@ -1,15 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services = {
-    gnome = { gnome-keyring = { enable = true; }; };
-
+    gnome = {
+      gnome-keyring = {
+        enable = true;
+      };
+    };
 
     xserver = {
       displayManager = {
         gdm.enable = true;
-        # autoLogin.enable = true;
-        # autoLogin.user = "ph";
       };
-      desktopManager = { gnome.enable = true; };
+      desktopManager = {
+        gnome.enable = true;
+      };
       enable = true;
       # displayManager.gdm.wayland = true;
       layout = "us";
@@ -31,53 +35,56 @@
     };
   };
 
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-    xterm
-  ]) ++ (with pkgs; [
-    cheese
-    # gedit
-    epiphany
-    geary
-    evince
-    gnome-music
-    gnome-terminal
-    gnome-characters
-    gnome-contacts
-    gnome-initial-setup
-    gnome-calculator
-    totem
-    tali
-    iagno
-    hitori
-    atomix
-    yelp
-  ]);
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      xterm
+    ])
+    ++ (with pkgs; [
+      cheese
+      # gedit
+      epiphany
+      geary
+      evince
+      gnome-music
+      gnome-terminal
+      gnome-characters
+      gnome-contacts
+      gnome-initial-setup
+      gnome-calculator
+      totem
+      tali
+      iagno
+      hitori
+      atomix
+      yelp
+    ]);
 
+  environment.systemPackages =
+    (with pkgs; [
+      arc-theme
+      ayu-theme-gtk
+      pop-gtk-theme
+      numix-gtk-theme
+      catppuccin-gtk
+      pop-icon-theme
+      tela-icon-theme
+      papirus-icon-theme
 
-  environment.systemPackages = (with pkgs; [
-    arc-theme
-    ayu-theme-gtk
-    pop-gtk-theme
-    numix-gtk-theme
-    catppuccin-gtk
-    pop-icon-theme
-    tela-icon-theme
-    papirus-icon-theme
+      wl-clipboard
 
-    wl-clipboard
-
-    dconf-editor
-    gnome-tweaks
-    adwaita-icon-theme
-    nordic
-    # nordzy-icon-theme
-    nordzy-cursor-theme
-  ]) ++ (with pkgs; [
-    gnome-tweaks
-    gnome-boxes
-    gnome-screenshot
-    gnome-shell-extensions
-  ]);
+      dconf-editor
+      gnome-tweaks
+      adwaita-icon-theme
+      nordic
+      # nordzy-icon-theme
+      nordzy-cursor-theme
+    ])
+    ++ (with pkgs; [
+      gnome-tweaks
+      gnome-boxes
+      gnome-screenshot
+      gnome-shell-extensions
+    ]);
 }
