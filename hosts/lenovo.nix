32f99f1a03b65e2e53166a557d4d2a4b551f4ef9
ph <chenyi.ph@outlook.com>
2025-04-18 20:17:45 +0800

    add lenvo host

---
+++ hosts/lenovo.nix
@@ -0,0 +1,48 @@
+inputs@{ nixpkgs-stable, ... }:
+let
+  system = "x86_64-linux";
+  username = "ilya";
+  hostName = "lenovo";
+  devEnv = {
+    nodejs = true;
+  };
+  specialArgs = inputs // {
+    inherit
+      system
+      username
+      devEnv
+      hostName
+      ;
+  };
+in
+nixpkgs-stable.lib.nixosSystem {
+  inherit specialArgs;
+  inherit system;
+
+  modules = [
+    ../hardwares/lenovo.nix
+
+    inputs.agenix.nixosModules.default
+    inputs.home-manager.nixosModules.home-manager
+    ../modules/hmModules.nix
+
+    # ../deps/aikami.nix
+    ../deps/agenix.nix
+    ../deps/cli.nix
+    ../deps/dae.nix
+    ../deps/dev.nix
+    ../deps/docker.nix
+    ../deps/fish.nix
+    ../deps/grub.nix
+    ../deps/home-manager.nix
+    ../deps/locale.nix
+    ../deps/misc.nix
+    ../deps/network.nix
+    ../deps/nixos.nix
+    ../deps/nixpkgs.nix
+    ../deps/starship.nix
+    ../deps/tmux.nix
+    ../deps/user.nix
+    ../deps/vim.nix
+  ];
+}
