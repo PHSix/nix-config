2527ec7852d2aa5f2a41e2dda558260987bd19cb
ph <chenyi.ph@outlook.com>
2025-07-04 10:05:28 +0800

    add gemini-cli

--- home/cli.nix
+++ home/cli.nix
@@ -1,37 +1,39 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    nix-tree
+    htop
+    wget
+    xsel
+    ripgrep
+    fzf
+    fd
+    delta
+    eza
+    tree
+    zoxide
+    sqlite
+    lazygit
+    ranger
+    yazi
+    w3m
+    jq
+    fx
+    neofetch
+    fastfetch
+    bat
+    just
+    nixpkgs-fmt
+    nix-output-monitor
+    unrar
+    biome
 
-      { pkgs, ... }:
-      {
-        home.packages = with pkgs; [
-          nix-tree
-          htop
-          wget
-          xsel
-          ripgrep
-          fzf
-          fd
-          delta
-          eza
-          tree
-          zoxide
-          sqlite
-          lazygit
-          ranger
-          yazi
-          w3m
-          jq
-          fx
-          neofetch
-          fastfetch
-          bat
-          just
-          nixpkgs-fmt
-          nix-output-monitor
-          unrar
+    # some nix cli utils tool
+    nurl
+    mpv
 
-          # some nix cli utils tool
-          nurl
-          mpv
+    # termusic
 
-          # termusic
-        ];
-      }
+    gemini-cli
+  ];
+}
